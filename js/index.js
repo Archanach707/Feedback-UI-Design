f5168c27126bd176560f9d62bae59b36235a288a
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-09 14:35:29 +0530

    making function

--- js/index.js
+++ js/index.js
@@ -13,6 +13,15 @@ panel.addEventListener('click', (e) => {
   }
 })
 
+sendBtn.addEventListener('click', (e) => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>
+  `
+})
 
 function removeActive() {
   for(let i = 1; i < ratings.length; i++) {
